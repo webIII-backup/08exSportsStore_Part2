9dfe36ed9aab6ec5ac599e9a55084d0c0fc53e06
Stefaan Samyn <stefaan.samyn@hogent.be>
2017-11-19 23:53:20 +0100

    Add project files.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Domain;
+using SportsStore.Models.ProductViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index(int categoryId = 0)
+        {
+            IEnumerable<Product> products;
+            if (categoryId == 0)
+                products = _productRepository.GetAll();
+            else
+                products = _productRepository.GetByCategory(categoryId);
+            products = products.OrderBy(b => b.Name).ToList();
+            ViewData["Categories"] = GetCategoriesSelectList(categoryId);
+            return View(products);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Product product = _productRepository.GetById(id);
+            if (product == null)
+                return NotFound();
+            ViewData["IsEdit"] = true;
+            ViewData["Categories"] = GetCategoriesSelectList();
+            return View(new EditViewModel(product));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, EditViewModel editViewModel)
+        {
+            Product product = _productRepository.GetById(id);
+            MapToProduct(editViewModel, product);
+            _productRepository.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            var product = new Product();
+            ViewData["IsEdit"] = false;
+            ViewData["Categories"] = GetCategoriesSelectList();
+            return View(nameof(Edit), new EditViewModel(product));
+        }
+
+        [HttpPost]
+        public IActionResult Create(EditViewModel editViewModel)
+        {
+            var product = new Product();
+            MapToProduct(editViewModel, product);
+            _productRepository.Add(product);
+            _productRepository.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+                return NotFound();
+            ViewData["ProductName"] = product.Name;
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Product product = _productRepository.GetById(id);
+            _productRepository.Delete(product);
+            _productRepository.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private SelectList GetCategoriesSelectList(int selected = 0)
+        {
+            return new SelectList(_categoryRepository.GetAll().OrderBy(g => g.Name).ToList(),
+                nameof(Category.CategoryId), nameof(Category.Name), selected);
+        }
+
+        private void MapToProduct(EditViewModel editViewModel, Product product)
+        {
+            product.Name = editViewModel.Name;
+            product.Description = editViewModel.Description;
+            product.Price = editViewModel.Price;
+            product.InStock = editViewModel.InStock;
+            product.Availability = editViewModel.Availability;
+            product.Category = _categoryRepository.GetById(editViewModel.CategoryId);
+        }
+    }
+}
---
+++ SportsStore/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Data;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models.Domain;
+using SportsStore.Data.Repositories;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=Index}/{id?}");
+            });
+
+            SportsStoreDataInitializer.InitializeData(dbContext);
+        }
+    }
+}
